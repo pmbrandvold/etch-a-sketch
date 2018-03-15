a1b3ba2c317e475efeffa482d2a052ada815a30f
Phillip Brandvold <phillip.brandvold@gmail.com>
2018-03-14 20:18:33 -0500

    Add function to change background of grid squares on mouseover.

--- main.js
+++ main.js
@@ -4,5 +4,10 @@ function paintGrid() {
   for (let i = 0; i < 16; i++) {
     let div = document.createElement('div');
     gridContainer.appendChild(div);
+    div.addEventListener('mouseover', function() {hoverColorChange(div)});
   }
+}
+
+function hoverColorChange(div) {
+  div.classList = 'hover';
 }
