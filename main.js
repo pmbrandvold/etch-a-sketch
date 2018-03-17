f85da84c369f7b0ba3b8a6aaae716b4d0ae6a00c
Phillip Brandvold <phillip.brandvold@gmail.com>
2018-03-17 10:03:05 -0500

    Write function for clearing the current grid when creating a new grid. Change variables for clarity.

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const canvas = document.getElementById('canvas');
 
-let playerChoice = '';
+let gridSideLength = 100;
 
 function paintGrid() {
-  for (let i = 0; i < playerChoice; i++) {
+  document.documentElement.style.setProperty(`--gridSize`, this.value = Math.sqrt(gridSideLength));
+  for (let i = 0; i < gridSideLength; i++) {
     let div = document.createElement('div');
     canvas.appendChild(div);
     div.addEventListener('mouseover', function() {hoverColorChange(div)});
@@ -15,7 +16,18 @@ function hoverColorChange(div) {
 }
 
 function customGrid() {
-  playerChoice = Number(prompt('How big should the grid be?'));
+  gridSideLength = Number(prompt('How big should the grid be?'));
+  clearCurrentGrid();
   paintGrid();
-  document.documentElement.style.setProperty(`--gridSize`, this.value = Math.sqrt(playerChoice));
+  document.documentElement.style.setProperty(`--gridSize`, this.value = Math.sqrt(gridSideLength));
 }
+
+function clearCurrentGrid() {
+    var elems = document.querySelectorAll('#canvas div');
+    elems.forEach((elem) => {(elem).classList.remove('hover')});
+}
+
+paintGrid();
+
+const newGridButton = document.getElementById('newGrid')
+newGridButton.addEventListener('click', customGrid);
